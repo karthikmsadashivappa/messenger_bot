45fa72c10a2edea1f2e3e472636d3d92f00c610f
Karthik M S <ms18karthik@gmail.com>
2018-02-02 17:38:54 +0530

    migrations are created and recieved and reply were generated

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,27 +2,47 @@ class CallbackController < ApplicationController
  protect_from_forgery with: :null_session
 
   def index
-  	if params["hub.verify_token"] == "asd"
-  		render json: params["hub.challenge"]
-  	end
+    if params["hub.verify_token"] == "asd"
+      render json: params["hub.challenge"]
+    end
   end
 
   def recieved_data
-  	therequest = request.body.read
-  	data = JSON.parse(therequest)
-  	enteries = data["entry"]
-  	enteries.each do |entry|
-  		entry["messaging"].each do |messaging|
-  			sender = messaging["sender"]["id"]
-  			text = messaging["message"]["text"]
-  			myjson = {"recipient": {"id": "#{sender}"},"message": {"text": "Hi welcome back"}}
-  			puts HTTP.post(url, json: myjson)
-  		end
-  	end
-  	render "recieved_data"
+    therequest = request.body.read
+    data = JSON.parse(therequest)
+    parse_data(data)
+    render "recieved_data"
   end
+
+  def parse_data(data)
+    enteries = data["entry"]
+    enteries.each do |entry|
+      entry["messaging"].each do |messaging|
+        sender = messaging["sender"]["id"]
+        text = messaging["message"]["text"]
+        analysis(sender, text.downcase)
+      end
+    end
+  end
+
+  def analysis(sender, text)
+    message = Message.where(:recieved => text).first
+    if message
+      reply = message.reply
+    else
+      reply = "Sorry not found"
+    end
+      send_message(sender, reply)
+  end
+
+  def send_message(sender, text)
+    myjson = {"recipient": {"id": "#{sender}"},"message": {"text": "#{text}"}}
+    puts HTTP.post(url, json: myjson)
+  end
+
+
   def url 
-  	"https://graph.facebook.com/v2.6/me/messages?access_token=EAACXKi9DHWIBAKZCeR9CSdbjUap1Bec53bFdBzZB48eq1iQDMKPmMH3UCvBpRuiH6AZA32ZC1ZAgq3zkRPOVK7ZCXJggsqr3kJRSnnlAZBHjbx7BQ2j4atStKJK6N0B9Tkp5U9gYYMAUZBni3hMFoRowhqYKFometSTwMYGaxRZBsFzPBfViHGVin"
+    "https://graph.facebook.com/v2.6/me/messages?access_token=EAACXKi9DHWIBAKZCeR9CSdbjUap1Bec53bFdBzZB48eq1iQDMKPmMH3UCvBpRuiH6AZA32ZC1ZAgq3zkRPOVK7ZCXJggsqr3kJRSnnlAZBHjbx7BQ2j4atStKJK6N0B9Tkp5U9gYYMAUZBni3hMFoRowhqYKFometSTwMYGaxRZBsFzPBfViHGVin"
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   get 'callback/index'
   post '/' => 'callback#recieved_data'
 
